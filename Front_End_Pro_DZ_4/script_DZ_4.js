b15d62891c7364d45e256706c56b901bc5ea53f3
stanislavk <supremewizard81@gmail.com.com>
2022-09-07 22:45:38 +0300

    hjhgfd

--- Front_End_Pro_DZ_4/script_DZ_4.js
+++ Front_End_Pro_DZ_4/script_DZ_4.js
@@ -3,6 +3,12 @@
 
 const inputAction = getInputAction();
 
+const calcAmount = getcalcAmount();
+
+const numAmount = getNumAmount('Insert Your Numbers:');
+
+const calcNumAmount = calculateNumAmount(calcAmount, inputAction);
+
 console.log(inputAction);
 
 
@@ -20,3 +26,45 @@ function isInputActionInvalid(calc) {
 }
 
 
+function getcalcAmount() {
+  let operand;
+  do {
+    operand = prompt('Insert Operands Amount');
+  } while (isIncorrectCalcAmount(operand));
+
+  return +operand;
+}
+
+
+function isIncorrectCalcAmount(check) {
+  return (
+    check === null || +check === 0 || check.trim() === "" 
+    || isNaN(check) || check < 2);
+}
+
+
+function getNumAmount() {
+  let num;
+
+  do{
+    num = prompt('Choose Your Numbers');
+  } while(isNumAmountInvalid(num));
+
+  return +num;
+}
+
+
+function isNumAmountInvalid(check) {
+  return (check === null || +check === 0 || 
+    check.trim() === ""  || isNaN(check));
+}
+
+
+function calculateNumAmount( num, inputAction) {
+  for (let i = 0; i < 2; i++) {
+    const numAmount = getNumAmount('Number:' + (i+1));
+
+    console.log(numAmount);
+  }
+
+}
