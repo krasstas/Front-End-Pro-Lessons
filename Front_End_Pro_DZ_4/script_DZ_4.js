ee470296821ec170f8c1b734fee8413a81dff8b0
stanislavk <supremewizard81@gmail.com.com>
2022-09-08 14:47:34 +0300

    Home work 4

--- Front_End_Pro_DZ_4/script_DZ_4.js
+++ Front_End_Pro_DZ_4/script_DZ_4.js
@@ -1,70 +1,87 @@
 "use strict";
 
 
+
+
 const inputAction = getInputAction();
 
-const calcAmount = getcalcAmount();
+const calcAmount = getCalcAmount();
 
-const numAmount = getNumAmount('Insert Your Numbers:');
+const finalResult = calculateFinalResult(calcAmount, inputAction);
 
-const calcNumAmount = calculateNumAmount(calcAmount, inputAction);
+console.log(finalResult);
 
-console.log(inputAction);
-
+alert(finalResult);
 
 function getInputAction() {
-  let calc;
-  do {
-    calc = prompt('Input Your Action:');
-  } while(isInputActionInvalid(calc));
+    let sign;
 
-  return calc;
+    do {
+        sign = prompt('Input Your Action:');
+    } while (isInputActionInvalid(sign));
+      console.log(`Your action is ${sign}`);
+      alert(`Your action is ${sign}`);
+
+    return sign;
 }
 
-function isInputActionInvalid(calc) {
-  return calc !== "+" && calc !== "-" && calc !== "*" && calc !== "/";
+function isInputActionInvalid(sign) {
+    return sign !== '+' && sign !== '-' && sign !== '*' && sign !== '/';
 }
 
-
-function getcalcAmount() {
-  let operand;
-  do {
-    operand = prompt('Insert Operands Amount');
-  } while (isIncorrectCalcAmount(operand));
-
-  return +operand;
+function getCalcAmount() {
+    return getNumber('Insert count of actions');
 }
 
-
-function isIncorrectCalcAmount(check) {
-  return (
-    check === null || +check === 0 || check.trim() === "" 
-    || isNaN(check) || check < 2);
+function isOperandAmountInvalid(check) {
+    return isNumberInvalid(check) || check < 2;
 }
 
-
-function getNumAmount() {
-  let num;
-
-  do{
-    num = prompt('Choose Your Numbers');
-  } while(isNumAmountInvalid(num));
-
-  return +num;
+function getOperand(label) {
+    return getNumber(label);
 }
 
+function getNumber(title) {
+    let num;
 
-function isNumAmountInvalid(check) {
-  return (check === null || +check === 0 || 
-    check.trim() === ""  || isNaN(check));
+    do {
+        num = prompt(title);
+    } while (isOperandInvalid(num));
+
+    return Number(num);
 }
 
+function isOperandInvalid(check) {
+    return isNumberInvalid(check);
+}
 
-function calculateNumAmount( num, inputAction) {
-  for (let i = 0; i < 2; i++) {
-    const numAmount = getNumAmount('Number:' + (i+1));
+function isNumberInvalid(check) {
+    return check === null || check.trim() === '' || isNaN(check);
+}
 
-    console.log(numAmount);
-  }
+function calculateFinalResult(amount, operator) {
+    let result = getOperand('Insert Number 1');
+    let expression = result;
 
+    for (let i = 2; i <= amount; i++) {
+        const operand = getOperand('Insert Number ' + i);
+
+        result = calcAction(result, operand, operator);
+        expression += ` ${operator} ${operand}`;
+    }
+
+    return `${expression} = ${result}`;
+}
+
+function calcAction(a, b, action) {
+    switch (action) {
+        case '+':
+            return a + b;
+        case '-':
+            return a - b;
+        case '*':
+            return a * b;
+        case '/':
+            return a / b;
+    }
 }
